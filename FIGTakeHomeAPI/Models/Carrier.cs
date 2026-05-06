648f30cd950922803370c752e2033323557bd7eb
Kieran Herr <kieranherr@gmail.com>
2026-05-06 12:23:50 -0400

    Add project files.

---
+++ FIGTakeHomeAPI/Models/Carrier.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FIGTakeHomeAPI.Models
+{
+    public class Carrier
+    {
+        [Key]
+        int Id { get; set; } 
+        string CarrierName { get; set; }
+        
+    }
+}
